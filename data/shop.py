ed34c7239e69f5254598720fd63cf27bc35a638b
Evgeniy <esinepolskij@gmail.com>
2024-04-30 06:46:10 +0300

    0.3

--- data/shop.py
+++ data/shop.py
@@ -1,16 +1,14 @@
-import datetime
 import sqlalchemy
-from sqlalchemy import orm
 
 from .db_session import SqlAlchemyBase
 
 
-class News(SqlAlchemyBase):
+class Shop(SqlAlchemyBase):
     __tablename__ = 'shop'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
     title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     info = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
-                                     default=datetime.datetime.now)
+    type = sqlalchemy.Column(sqlalchemy.String)
+    price = sqlalchemy.Column(sqlalchemy.Integer)
--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template, redirect
 from data import db_session
-from flask_login import LoginManager, login_user, login_manager
+from flask_login import LoginManager, login_user, login_manager, login_required, logout_user, current_user
 
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, SubmitField, EmailField, BooleanField
 from wtforms.validators import DataRequired
 
 from data.users import User
+from data.shop import Shop
 from forms.user import RegisterForm
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 class LoginForm(FlaskForm):
@@ -20,10 +26,8 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Войти')
 
 
-
-
 @app.route('/register', methods=['GET', 'POST'])
-def reqister():
+def register():
     form = RegisterForm()
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
@@ -38,13 +42,25 @@ def reqister():
         user = User(
             name=form.name.data,
             email=form.email.data,
-            about=form.about.data
         )
         user.set_password(form.password.data)
         db_sess.add(user)
         db_sess.commit()
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/cart')
+def cart():
+    if current_user.is_authenticated:
+        user_id = current_user.id
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).get(user_id)
+        user_bag = user.bag.split(';') if user.bag else []
+
+        return render_template('cart.html', user_bag=user_bag)
+    else:
+        return "Пожалуйста, войдите на сайт, чтобы просмотреть корзину."
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -62,17 +78,46 @@ def login():
     return render_template('login.html', title='Авторизация', form=form)
 
 
-# @login_manager.user_loader
+@login_manager.user_loader
 def load_user(user_id):
     db_sess = db_session.create_session()
     return db_sess.query(User).get(user_id)
 
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/add_to_cart/<int:id>')
+def add(id):
+    if current_user.is_authenticated:
+        user_id = current_user.id
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).get(user_id)
+        user.bag = user.bag.split(';') if user.bag else ''
+        user.bag = user.bag + f' {id};'
+        db_sess.commit()
+
+        return redirect("/")
+    else:
+        return "Пожалуйста, войдите на сайт, чтобы просмотреть корзину."
+
+
+@app.route('/')
+def index():
+    db_sess = db_session.create_session()
+    shop_items = db_sess.query(Shop).all()
+    global shop_data
+    shop_data = [[item.id, item.title, item.info.split(';'), item.type, item.price] for item in shop_items]
+    return render_template('base.html', shop=shop_data)
+
+
 def main():
     db_session.global_init("db/main.sqlite")
     app.run()
-    login_manager = LoginManager()
-    login_manager.init_app(app)
 
 
 if __name__ == '__main__':
